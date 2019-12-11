cca2a440129d210df865dbc9414768d8b272ea38
Aram Vanyan <aram.vanyan.a@gmail.com>
2019-12-12 01:18:21 +0400

    add CheckForSeats

---
+++ src/CheckForSeats.java
@@ -0,0 +1,12 @@
+public class CheckForSeats {
+    private static int [][]matrixOfSeats = new int[10][7];
+    // A1B2
+    public static void takeSeats(String string){
+        String stringWithoutSpaces = string.trim();
+        for (int i = 0; i <stringWithoutSpaces.length() ; i+=2) {
+
+        }
+
+    }
+
+}
