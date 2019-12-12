47ab938abf77d86c155a38c09099a1b1577ae681
Aram Vanyan <aram.vanyan.a@gmail.com>
2019-12-12 12:24:31 +0400

    add final changes

--- src/CheckForSeats.java
+++ src/CheckForSeats.java
@@ -2,31 +2,40 @@ import java.sql.SQLOutput;
 
 public class CheckForSeats {
     private static int [][]matrixOfSeats = new int[10][6];
-
     public static int[][] getMatrixOfSeats() {
         return matrixOfSeats;
     }
-
     public static void setMatrixOfSeats(int[][] matrixOfSeats) {
         CheckForSeats.matrixOfSeats = matrixOfSeats;
     }
-
-    // A1B2
     public static void takeSeats(String string){
         String stringWithoutSpaces = string.trim();
         for (int i = 0; i <stringWithoutSpaces.length() ; i+=2) {
             int column = stringWithoutSpaces.charAt(i)-65;
             int row = Integer.parseInt(stringWithoutSpaces.substring(i+1,i+2))-1;
             matrixOfSeats[row][column] = 1;
-
         }
     }
     public static void checkForFreeSeats(){
         for (int i = 0; i < matrixOfSeats.length; i++) {
+            System.out.print("Free pairs of seat in row " + (int)(i+1) + " " + "are  ");
             for (int j = 0; j <matrixOfSeats[i].length ; j+=3) {
-                if((matrixOfSeats[i][j]|matrixOfSeats[i][j+1])==0)System.out.print((char)(j+65) + " " + i+1 + " ");
-                if((matrixOfSeats[i][j]|matrixOfSeats[i][j+2])==0)System.out.print((char)(j+65) + " " + i+1 + " ");
-                if((matrixOfSeats[i][j+1]|matrixOfSeats[i][j+2])==0)System.out.print((char)(j+65) + " " + i+1 + " ");
+                if((matrixOfSeats[i][j]|matrixOfSeats[i][j+1])==0)
+                {
+                    System.out.print((char)(j+65) + " " + (int)(i + 1) + " ");
+                    System.out.print((char)(j+1+65) + " " + (int)(i + 1) + " ");
+                    System.out.print("  ");
+                }
+                if((matrixOfSeats[i][j]|matrixOfSeats[i][j+2])==0) {
+                    System.out.print((char)(j + 65) + " " + (int) (i + 1) + " ");
+                    System.out.print((char)(j+2+65) + " " + (int)(i + 1) + " ");
+                    System.out.print("  ");
+                }
+                if((matrixOfSeats[i][j+1]|matrixOfSeats[i][j+2])==0){
+                    System.out.print((char)(j+1+65) + " " + (int)(i + 1) + " ");
+                    System.out.print((char)(j+2+65) + " " + (int)(i + 1) + " ");
+                    System.out.print("  ");
+                }
 
             }
             System.out.println();
@@ -45,7 +54,7 @@ public class CheckForSeats {
 
     public static void main(String[] args) {
         int [][]matrixOfSeats = new int[10][7];
-        String string = new String("A1B4C3");
+        String string = new String("A1B4C3D6");
         takeSeats(string);
         printMatrix(getMatrixOfSeats());
         checkForFreeSeats();
